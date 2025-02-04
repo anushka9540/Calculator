6a40d48fbfc3145683e059314b2f8e26eb060b35
AnushkaPrajapati <anushkaprajapati827@gmail.com>
2025-02-04 15:31:37 +0530

    implemented cal

--- script1.js
+++ script1.js
@@ -17,20 +17,29 @@ buttons.forEach((button) => {
         input.value = 'Error';
         string = '';
       }
-    } else if (value === 'AC') {
+    } 
+    
+    else if (value === 'AC') {
       string = '';
       input.value = string;
-    } else if (value === 'DEL') {
+    } 
+    
+    else if (value === 'DEL') {
       string = string.substring(0, string.length - 1);
       input.value = string;
-    } else if (operators.includes(value)) {
+    }
+    
+    else if (operators.includes(value)) {
       if (string !== '' && !operators.includes(lastChar)) {
         string += value;
-      } else if (string !== '' && operators.includes(lastChar)) {
+      } 
+      else if (string !== '' && operators.includes(lastChar)) {
         string = string.slice(0, -1) + value;
       }
       input.value = string;
-    } else {
+    } 
+    
+    else {
       string += value;
       input.value = string;
     }
